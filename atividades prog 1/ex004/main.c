d3d48132ffc0689165e11997e6b28722ebc77677
Ozeias Moreira <OzMoreira17@gmail.com>
2023-04-26 09:28:56 -0300

    Add files via upload

---
+++ atividades prog 1/ex004/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int horas,min,seg;
+    long int result_em_segundos;
+    printf("\nDiga que horas, quantos minutos e quantos segundos são respectivamente: ");
+    scanf("%d%d%d" ,&horas,&min,&seg);
+
+    horas=60*60*horas;
+    min=60*min;
+
+    printf("O resultado em segundos e de %d \n",horas+min+seg);
+
+   system("PAUSE");
+}
